69d067492f877d4d3beeec5e97fa03bc9b17dc1d
cyc2303 <cyc0723@naver.com>
2020-06-21 23:30:29 +0900

    Add Function: getRecommendedStatusBarTextColor

--- lib/flutter_status_bar_color.dart
+++ lib/flutter_status_bar_color.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:ui';
 
 import 'package:flutter/services.dart';
 
@@ -6,8 +7,13 @@ class FlutterStatusBarColor {
   static const MethodChannel _channel =
       const MethodChannel('flutter_status_bar_color');
 
-  static Future<String> get platformVersion async {
-    final String version = await _channel.invokeMethod('getPlatformVersion');
-    return version;
+  static Color getRecommendedStatusBarTextColor(Color color){
+    var r = color.red;
+    var g = color.green;
+    var b = color.blue;
+
+    var luma = 0.2126 * r + 0.7152 * g + 0.0722 * b;
+
+    return luma > 40 ? Color(0xff000000) : Color(0xffffffff);
   }
 }
